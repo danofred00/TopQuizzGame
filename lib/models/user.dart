832b753ed6f12a2aa889c6a029b7ca8bd396ba26
Daniel Leussa <leussadaniel04@gmail.com>
2022-09-16 21:13:45 +0200

    Adding question_count property to User object

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,13 +1,19 @@
 
+/**
+ * This class represent my User Object
+ */
 class User {
 
   String name;
   int score;
+  int question_count = 0;
 
-  User(this.name, this.score);
+  User(this.name, this.score, {int question_count = 0}) {
+    this.question_count = question_count;
+  }
 
   @override
   String toString() {
-    return "<User ${name} - ${score}>";
+    return "<User ${name} - ${score} - ${question_count}>";
   }
 }
